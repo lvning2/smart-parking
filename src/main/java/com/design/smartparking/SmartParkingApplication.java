03fa65a8d5c19ea08d24ddcbab8c850387cbf402
lvning2 <1261751961@qq.com>
2021-03-02 14:26:55 +0800

    跨域

--- src/main/java/com/design/smartparking/SmartParkingApplication.java
+++ src/main/java/com/design/smartparking/SmartParkingApplication.java
@@ -2,7 +2,11 @@ package com.design.smartparking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +14,23 @@ public class SmartParkingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartParkingApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("Response-Status");
+        config.addExposedHeader("Response-Message");
+        config.addExposedHeader("Task-State");
+        config.addAllowedMethod("*");
+        config.setMaxAge(18000L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
